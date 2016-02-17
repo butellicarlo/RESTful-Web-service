2dc6b7fd720f4a187e677bf1fd96c4d957d37d61
carlitos26 <root@Carlos-MacBook-Pro.local>
2016-02-17 12:37:27 +0100

    Third point done

--- scripts/app.py
+++ scripts/app.py
@@ -15,33 +15,32 @@ cur = conn.cursor()
 def api_root():
 	return '{}\n'.format('Welcome')
 
-# $curl --request GET http://127.0.0.1:5000/entries
+# $ curl --request GET http://127.0.0.1:5000/entries
 @app.route('/entries', methods = ['GET'])
 def api_entries():
-	query = cur.execute("SELECT count(*) FROM names;")
+	query = cur.execute("SELECT count(*) FROM names;").fetchone()
 	conn.commit()
-	entries = query.fetchone()
-	return '{}\n'.format('{}\n'.format(entries)[1:-3])
+	return '{}\n'.format('{}\n'.format(query)[1:-3])
 	
-# $curl --request GET http://127.0.0.1:5000/entry_name?name=Annie
-@app.route('/entry_name', methods = ['GET'])
-def api_entry_name():
-    query = cur.execute("SELECT id, year, gender, count FROM names WHERE name = '%s';" % request.args['name'])
+# $ curl --request GET http://127.0.0.1:5000/entry_name?name=Annie
+@app.route('/entry_name/<name>', methods = ['GET'])
+def api_entry_name(name):
+    query = cur.execute("SELECT id, year, gender, count FROM names WHERE name = '%s';" % name)
     conn.commit()
     entries = [dict({'id': row[0], 'year': row[1], 'gender': row[2], 'count': row[3]}) for row in query.fetchall()]
-    return jsonify({'Entries for %s' % request.args['name']: entries})
+    return jsonify({'Entries for %s' % name: entries})
 
-@app.route('/insert')
-def api_insert():
-    #query = 'INSERT INTO names (name, year, gender, count) VALUES ( ?, ?, ?, ? )', (request.args['name'], request.args['year'], request.args['gender'], request.args['count'])
-    cur.execute('INSERT INTO names (name, year, gender, count) VALUES ( "Carlo", 1984, "M", 12 )')
+# $ curl --request POST http://127.0.0.1:5000/insert/Ronnie,1987,M,102
+@app.route('/insert/<name>,<year>,<gender>,<count>', methods = ['POST'])
+def api_insert(name,year,gender,count):
+    cur.execute('INSERT INTO names (name, year, gender, count) VALUES (?,?,?,?)', (name,year,gender,count))
     conn.commit()
     id = cur.lastrowid
-    #c.close()
-    return id
+    return '{}\n'.format(str(id))
 
 #End view
 
 if __name__ == '__main__':
 	#Run the Server
-	app.run(debug=True)
+    #app.run(debug=True)
+    app.run()
